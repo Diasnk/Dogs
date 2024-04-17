50d9ac404842568ee5e917998c84adc1d3977d9a
Diasnk <diasnugumanov258@gmail.com>
2024-04-17 09:56:48 +0500

    chat design

--- src/app/specialist/page.tsx
+++ src/app/specialist/page.tsx
@@ -5,7 +5,7 @@ export default function Specialist() {
         <>
             <div className="bpict text- flex min-h-screen w-screen flex-col items-center justify-between p-24">
                 <div className='p-3 w-[950px] rounded-md '>
-                    <h2 className='text-2xl font-semibold'>Chat with online AI Guide</h2>
+                    <h2 className='text-2xl font-semibold'>Chat with online AI Specialist</h2>
                     <ChatComponent/>
                 </div>
             </div>
